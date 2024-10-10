0c06daf2c67229487f771c666786b6a81a3f0ae8
Jacky Wu <jacky.wu@heidi.cloud>
2024-10-10 14:36:24 -0700

    Get site URL from env

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,8 @@
 import authorAvatar from "../../public/images/author/devbertskie.png";
 export const siteConfig = {
+  baseURL: process.env.NEXT_PUBLIC_SITE_URL,
   name: "Wild Runner",
+  title: "野馬營",
   description: "一切關於跑步，越野的事情",
   author: "追雲逐雪",
   authorImage: authorAvatar,
