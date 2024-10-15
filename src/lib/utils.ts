bad55535bff213031fe2d19041b34bb3ac973ce3
Jacky Wu <jacky.wu@heidi.cloud>
2024-10-14 19:39:03 -0500

    Add fetcher to utilies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -70,3 +70,8 @@ export function calculateDisplayedDimensions(
     displayedHeight: displayedHeight,
   };
 }
+
+export function fetcher<JSON = any>(input: string, init?: any): Promise<JSON> {
+  if (/^\//.test(input)) input = process.env.NEXT_PUBLIC_SITE_URL! + input;
+  return fetch(input, init).then((res) => res.json());
+}
