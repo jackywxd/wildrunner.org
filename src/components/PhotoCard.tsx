6ad1bf81bcee9eee9c371bfbb909046c6efc48ee
Jacky Wu <jacky.wu@heidi.cloud>
2024-10-19 15:55:06 -0700

    Fix linting error

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -24,7 +24,7 @@ const PhotoCard: React.FC<IPhotoCardProps> = ({
   className,
 }) => {
   const { src, slug, width, height, blurDataURL } = photo;
-  const [isInView, setIsInView] = useState(isMobile ? true : false);
+  const [isInView, setIsInView] = useState(isMobile);
 
   const cardRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLImageElement>(null);
