e62c316c5838462547a7caf3a527635a29b90d8b
Jacky Wu <jacky.wu@heidi.cloud>
2024-10-20 14:47:07 -0700

    Remove velite build from nextjs - need to manually run velite cli - npx velite build to build - npx velite watch to watch

--- next.config.mjs
+++ next.config.mjs
@@ -17,21 +17,21 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin());
+    // config.plugins.push(new VeliteWebpackPlugin());
     return config;
   },
 };
 
-class VeliteWebpackPlugin {
-  static started = false;
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
-    });
-  }
-}
+// class VeliteWebpackPlugin {
+//   static started = false;
+//   apply(/** @type {import('webpack').Compiler} */ compiler) {
+//     compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+//       if (VeliteWebpackPlugin.started) return;
+//       VeliteWebpackPlugin.started = true;
+//       const dev = compiler.options.mode === "development";
+//       await build({ watch: dev, clean: !dev });
+//     });
+//   }
+// }
 
 export default nextConfig;
